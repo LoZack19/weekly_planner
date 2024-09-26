69ac6103305fa871125eb09aa581f769e636aefd
LoZack19 <giozaccaria02@gmail.com>
2024-09-26 16:45:39 +0200

    new plan

--- bin/generate_plan.rs
+++ bin/generate_plan.rs
@@ -1,26 +1,24 @@
 use std::fs::File;
 use std::io::{self, BufWriter};
+use std::str::FromStr;
 
 use weekly_planner::weekplan::{Time, Weekday};
-use weekly_planner::WeekPlan;
+use weekly_planner::{poli_plan, WeekPlan};
 
 fn main() -> Result<(), io::Error> {
     let start = Time::new(8, 30).unwrap();
-    let week_plan = {
-        let mut w = WeekPlan::new(start, 90, 7).unwrap();
-        w.try_insert_range(
-            Weekday::Monday,
-            (Time::new(10, 0).unwrap(), 2),
-            "Computer architectures".to_owned(),
-        )
-        .unwrap()
-        .try_insert_range(
-            Weekday::Tuesday,
-            (Time::new(8, 30).unwrap(), 2),
-            "Computer architectures".to_owned(),
-        )
-        .unwrap();
-        w
+    let week_plan = poli_plan! {
+        start, 90, 7,
+        "Monday" => "10:00", 2, "Computer architectures",
+        "Tuesday" => "8:30", 2, "Computer architectures",
+        "Tuesday" => "11:30", 2, "Electronics",
+        "Wednesday" => "8:30", 2, "Electronics",
+        "Thursday" => "11:30", 1, "Electronics",
+        "Monday" => "16:00", 1, "Operating systems",
+        // "Thursday" => "11:30", 1, "Operating systems",
+        "Friday" => "8:30", 2, "Operating systems",
+        "Thursday" => "13:00", 1, "Simulation",
+        "Friday" => "14:30", 1, "Simulation",
     };
 
     println!("Writing to data/plan.json");
